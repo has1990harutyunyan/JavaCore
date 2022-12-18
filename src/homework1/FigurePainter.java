45daf63a124b020c68f9b93493b8974e2171c572
Hasmik <hasmik90harutyunyan@gmail.com>
2022-12-19 02:26:41 +0900

    Homework1

--- src/homework1/FigurePainter.java
+++ src/homework1/FigurePainter.java
@@ -4,7 +4,8 @@ public class FigurePainter {
     public static void main(String[] args) {
 
         /*
-        Printing a half pyramid
+        Printing a half pyramid normal e vonc vor te ha ձևի համաի մի բան փոխեք commit areq eli
+
         #
         ##
         ###
