f9c6c0aec17b8269744eb7a3f74ee7db7d944c87
Hasmik <hasmik90harutyunyan@gmail.com>
2022-12-19 01:01:10 +0900

    Homework1

---
+++ src/homework1/FigurePainter.java
@@ -0,0 +1,111 @@
+package homework1;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+
+        /*
+        Printing a half pyramid
+        #
+        ##
+        ###
+        ####
+        #####
+        * */
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+
+
+        System.out.println();
+       /*
+       Printing an inverted half pyramid
+       #####
+       ####
+       ###
+       ##
+       #
+       * */
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j >= i; j--) {
+                System.out.print("#");
+            }
+            System.out.println();
+
+        }
+        /*
+        Printing a right side half pyramid
+            #
+           ##
+          ###
+         ####
+        #####
+        * */
+        int num = 4;
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = num; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("#");
+
+            }
+            System.out.println();
+        }
+
+
+        /*
+        Printing a half pyramid with numbers
+        1
+        1 2
+        1 2 3
+        1 2 3 4
+        1 2 3 4 5
+        * */
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+
+            }
+            System.out.println();
+
+
+        }
+
+        System.out.println();
+
+        /*
+        Printing a full inverted pyramid
+        * * * * * * *
+         * * * * * *
+          * * * * *
+           * * * *
+            * * *
+             * *
+              *
+        * */
+
+        for (int i = 1; i <= 7; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+
+            }
+            for (int k = 7; k >= i; k--) {
+                System.out.print("* ");
+            }
+
+
+            System.out.println();
+
+        }
+
+    }
+
+
+}
---
+++ src/homework1/IfForExample.java
@@ -0,0 +1,48 @@
+package homework1;
+
+public class IfForExample {
+
+
+    public static void main(String[] args) {
+
+        // Finding max value between two variables
+
+        int a = 15;
+        int b = 45;
+
+        if (a < b) {
+            System.out.println("The variable \'a' is smaller than \'b'. " + "The value of \'b' is " + b + " .");
+        } else {
+            System.out.println("The variable \'a' is greater than \'b'. " + "The value of \'a' is " + a + " .");
+        }
+
+        // Printing values between two given variables
+
+        for (int i = a; i <= b; i++) {
+            if (i < b) {
+                System.out.print(i + ",");
+            } else {
+                System.out.print(i);
+            }
+        }
+        System.out.println();
+
+        // Casting int variables into char type and printing their values
+
+        char aCast = (char) a;
+        System.out.println("Tha value of aCast is " + aCast);
+        char bCast = (char) b;
+        System.out.println("Tha value of bCast is " + bCast);
+
+        // Checking equality between two variables
+
+        if (a == b) {
+            System.out.println("a is equal to b");
+        } else {
+            System.out.println("a is not equal to b");
+        }
+
+    }
+
+
+}
